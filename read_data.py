55e157a8e7c7db86835fe72decbe5b658dca4ab8
David Garza <david.garzas@udem.edu>
2024-10-30 17:06:46 +0100

    v1

--- read_data.py
+++ read_data.py
@@ -1,6 +1,5 @@
 # import OS
 import os
-import matplotlib.axes as axes
 import matplotlib.pyplot as plt
 
 cent_1 = []
@@ -10,36 +9,22 @@ cent_20 = []
 cent_50 = []
 cent_100 = []
 cent_200 = []
-data = []
-
-numbers = []
-file_2 = cent_1[1]
-for line in open(file_2):
-    number = line.split(',')
-    for i in range(len(number)):
-        number[i] = float(number[i])
-    numbers += number
-    xs = [x/204000.0 for x in range(len(numbers))]
-    plt.plot(xs, numbers, label=file_2)
     
-for x in os.listdir():
+for x in os.listdir("data_raw/"):
     if x.startswith("1_"):
-        cent_1 += [("1",x.open())]
+        cent_1 += [x]
     elif x.startswith("2_"):
-        cent_2 += [("2",x.open())]
+        cent_2 += [x]
     elif x.startswith("5_"):
-        cent_5 += [("5",x.open())]
+        cent_5 += [x]
     elif x.startswith("20_"):
-        cent_20 += [("20",x.open())]
+        cent_20 += [x]
     elif x.startswith("50_"):
-        cent_50 += [("50",x.open())]
+        cent_50 += [x]
     elif x.startswith("100_"):
-        cent_100 += [("100",x.open())]
+        cent_100 += [x]
     elif x.startswith("200_"):
-        cent_200 += [("200",x.open())]
-
-    #elem = (,x[0:x.find("_")])
-    #data.append(elem)
+        cent_200 += [x]
 
 print("Audios Cent 1: ", len(cent_1))
 print("Audios Cent 2: ", len(cent_2))
@@ -54,7 +39,7 @@ print("Audios Total: ",sum(audios))
 
 numbers = []
 file_1 = cent_1[0]
-for line in open(file_1):
+for line in open("data_raw/" + file_1):
     number = line.split(',')
     for i in range(len(number)):
         number[i] = float(number[i])
@@ -64,7 +49,7 @@ for line in open(file_1):
 
 numbers = []
 file_2 = cent_1[1]
-for line in open(file_2):
+for line in open("data_raw/" + file_2):
     number = line.split(',')
     for i in range(len(number)):
         number[i] = float(number[i])
@@ -74,7 +59,7 @@ for line in open(file_2):
 
 numbers = []
 file_3 = cent_1[2]
-for line in open(file_3):
+for line in open("data_raw/" + file_3):
     number = line.split(',')
     for i in range(len(number)):
         number[i] = float(number[i])
@@ -84,7 +69,7 @@ for line in open(file_3):
 
 numbers = []
 file_4 = cent_1[3]
-for line in open(file_4):
+for line in open("data_raw/" + file_4):
     number = line.split(',')
     for i in range(len(number)):
         number[i] = float(number[i])
@@ -94,7 +79,7 @@ for line in open(file_4):
 
 numbers = []
 file_5 = cent_1[4]
-for line in open(file_5):
+for line in open("data_raw/" + file_5):
     number = line.split(',')
     for i in range(len(number)):
         number[i] = float(number[i])
@@ -104,7 +89,7 @@ for line in open(file_5):
 
 numbers = []
 file_6 = cent_1[5]
-for line in open(file_6):
+for line in open("data_raw/" + file_6):
     number = line.split(',')
     for i in range(len(number)):
         number[i] = float(number[i])
@@ -114,7 +99,7 @@ for line in open(file_6):
 
 numbers = []
 file_7 = cent_1[6]
-for line in open(file_7):
+for line in open("data_raw/" + file_7):
     number = line.split(',')
     for i in range(len(number)):
         number[i] = float(number[i])
